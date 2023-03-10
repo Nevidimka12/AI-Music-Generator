47e54555bd6786f2e1f07712b71e1bf228725ae6
Alexander <47082891+StrangeAlex@users.noreply.github.com>
2023-03-10 19:13:56 +0300

    Create chord_classifier.py

---
+++ chord_classifier.py
@@ -0,0 +1,14 @@
+from chord_extractor.extractors import Chordino
+
+def transform_song_to_data(filename):
+
+  chordino = Chordino()
+  chords = chordino.extract(filename)
+  data = []
+  prev_len = 0
+
+  for chord in chords:
+    data.append((chord[0], chord[1]-prev_len))
+    prev_len = chord[1]
+
+  return data # contains elements in format (chord, duration)

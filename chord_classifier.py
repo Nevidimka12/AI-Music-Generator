9e762e6dfaa53c397be98452fccd5cefc3e72665
Alexander <47082891+StrangeAlex@users.noreply.github.com>
2023-03-13 22:03:49 +0300

    Update chord_classifier.py

--- chord_classifier.py
+++ chord_classifier.py
@@ -1,14 +1,16 @@
 from chord_extractor.extractors import Chordino
+import mingus.core.chords as chords
+
 
 def transform_song_to_data(filename):
 
-  chordino = Chordino()
-  chords = chordino.extract(filename)
-  data = []
-  prev_len = 0
+    chordino = Chordino()
+    chords = chordino.extract(filename)
+    data = []
+    prev_len = 0
 
-  for chord in chords:
-    data.append((chord[0], chord[1]-prev_len))
-    prev_len = chord[1]
+    for chord in chords:
+        data.append((chord[0], chord[1]-prev_len))
+        prev_len = chord[1]
 
-  return data # contains elements in format (chord, duration)
+    return data  # contains elements in format (chord, duration)

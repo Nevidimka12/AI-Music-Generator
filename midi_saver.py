a40faa7333f1c4f31bd2569f82f7ff2212cf439e
Alexander <47082891+StrangeAlex@users.noreply.github.com>
2023-03-28 23:43:22 +0300

    Create midi_saver.py

---
+++ midi_saver.py
@@ -0,0 +1,33 @@
+from mingus.core import chords
+import librosa
+from util_functions import decompose_chord
+
+
+def decompose_chord(chord):
+    if chord != 'N':  # 'N' means no chord is played
+        return chords.from_shorthand(chord)  # transforming string chord into array of string notes
+    else:
+        return []
+     
+    from midiutil import MIDIFile
+
+
+def save_chords_as_midi(chord_data, BPM=60):
+
+    track = 0
+    channel = 0
+    time = 0  # In beats
+    volume = 100  # 0-127, as per the MIDI standard
+    midi_file = MIDIFile(4)
+    midi_file.addTempo(track, time, BPM)
+
+    for chord, duration in chord_data:
+        decomposed_chord = decompose_chord(chord)
+        beat_duration = duration * (BPM/60)
+        # beat_duration = duration
+        for note in decomposed_chord:
+            midi_file.addNote(track, channel, librosa.note_to_midi(note + '3'), time, beat_duration, volume)
+        time += beat_duration
+
+    with open("result.mid", "wb") as output_file:
+        midi_file.writeFile(output_file)

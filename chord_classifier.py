dc356d18fb3ca7757bc4ad8a98c98cd4fbbb4d56
Alexander <47082891+StrangeAlex@users.noreply.github.com>
2023-03-28 23:52:38 +0300

    Update chord_classifier.py

--- chord_classifier.py
+++ chord_classifier.py
@@ -1,5 +1,10 @@
 from chord_extractor.extractors import Chordino
 
+def detect_BPM(song_name):
+    y, sr = librosa.load(song_name)
+    y_percussive = librosa.effects.percussive(y)
+    tempo, _ = librosa.beat.beat_track(y=y_percussive, sr=sr)
+    return tempo
 
 def transform_song_to_data(filename):
 
@@ -12,10 +17,4 @@ def transform_song_to_data(filename):
         data.append((chord[0], chord[1]-prev_len))
         prev_len = chord[1]
 
-    return data  # contains elements in format (chord, duration)
-
-def detect_BPM(song_name):
-    y, sr = librosa.load(song_name)
-    y_percussive = librosa.effects.percussive(y)
-    tempo, _ = librosa.beat.beat_track(y=y_percussive, sr=sr)
-    return tempo
+    return data, detect_BPM(filename)  # data contains elements in format (chord, duration)

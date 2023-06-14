29f66bd6a9693c8c221a73ae846f7d02104db11f
Nevidimka12 <81490205+Nevidimka12@users.noreply.github.com>
2023-06-14 17:06:18 +0300

    final_change_midi_saver.py

--- midi_saver.py
+++ midi_saver.py
@@ -1,6 +1,6 @@
+from midiutil import MIDIFile
 from mingus.core import chords
 import librosa
-from midiutil import MIDIFile
 
 
 def decompose_chord(chord):
@@ -8,10 +8,9 @@ def decompose_chord(chord):
         return chords.from_shorthand(chord)  # transforming string chord into array of string notes
     else:
         return []
-     
 
-def save_chords_as_midi(chord_data, BPM=60):
 
+def save_chords_as_midi(chord_data, BPM=60, transpose=0):
     track = 0
     channel = 0
     time = 0  # In beats
@@ -21,11 +20,43 @@ def save_chords_as_midi(chord_data, BPM=60):
 
     for chord, duration in chord_data:
         decomposed_chord = decompose_chord(chord)
-        beat_duration = duration * (BPM/60)
+        beat_duration = duration * (BPM / 60)
         # beat_duration = duration
         for note in decomposed_chord:
-            midi_file.addNote(track, channel, librosa.note_to_midi(note + '3'), time, beat_duration, volume)
+            midi_file.addNote(track, channel, librosa.note_to_midi(note + '3')+transpose, time, beat_duration, volume)
         time += beat_duration
 
     with open("result.mid", "wb") as output_file:
         midi_file.writeFile(output_file)
+
+
+def save_song_as_midi(chord_data, melody_data, BPM=60, transpose=0, melody_volume=100, chords_volume=100):
+
+    chords_track = 0
+    channel = 0
+    time = 0  # In beats
+    midi_file = MIDIFile(4)
+    midi_file.addTempo(chords_track, time, BPM)
+
+    for chord, duration in chord_data:
+        decomposed_chord = decompose_chord(chord)
+        beat_duration = duration * (BPM / 60)
+        # beat_duration = duration
+        for note in decomposed_chord:
+            midi_file.addNote(chords_track, channel, librosa.note_to_midi(note + '3')+transpose, time, beat_duration, chords_volume)
+        time += beat_duration
+
+    melody_track = 1
+    channel = 0
+    time = 0  # In beats
+    midi_file.addTempo(melody_track, time, BPM)
+
+    for note, duration in melody_data:
+        beat_duration = duration * (BPM / 60)
+        if note != 'N':
+            midi_file.addNote(melody_track, channel, librosa.note_to_midi(note)+transpose, time, beat_duration, melody_volume)
+        time += beat_duration
+
+    with open("result.mid", "wb") as output_file:
+        midi_file.writeFile(output_file)
+        
